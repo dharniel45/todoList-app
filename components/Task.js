ac853a3f9ae515b6923474b4bed1016e5d8bfbed
Daniel Idowu <74189249+dharniel45@users.noreply.github.com>
2021-09-08 02:35:18 -0700

    Add files via upload

---
+++ components/Task.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+
+
+const Task = (props) => {
+
+    return  (
+        <View style={styles.item}>
+            <View style={styles.itemLeft}>
+                <TouchableOpacity style={styles.square}></TouchableOpacity>
+                <Text style={styles.itemText}>{props.text}</Text>
+            </View>
+            <View style={styles.circular}></View>
+        </View>
+    )
+}
+
+
+const styles = StyleSheet.create({
+item: {
+    backgroundColor: '#FFF',
+    padding: 15,
+    borderRadius: 10,
+
+},
+itemLeft: {
+
+}
+square: {
+   width: 24,
+   height: 24,
+   backgroundColor: '#55BCF6', 
+   opacity: 0.4,
+}
+itemText: {
+
+}
+circular: {
+
+}
+
+});
+
+export default Task;
+
